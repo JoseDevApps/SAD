f8fc41a929e519780fa66fecdb91c94488a9b911
JoseDevApps <Lionhard_2.6@hotmail.com>
2024-02-02 11:39:37 -0400

    extract data updated

--- app/api/models.py
+++ app/api/models.py
@@ -6,6 +6,6 @@ class User(AbstractUser):
     email = models.CharField(max_length=255, unique=True)
     password = models.CharField(max_length=255)
     username = None
-
+    
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
--- codigo/Extract.py
+++ codigo/Extract.py
@@ -1,31 +1,23 @@
+'''
+Codigo de extraccion de datos
+'''
 import pandas as pd
-import io
 import numpy as np
-# funciones
-def verify_date(fn):
-    df = pd.read_excel(fn)
-    muestra = df.head(40)
-    objeto = {muestra.iloc[:,d].dtype == np.dtype(object) for d in range(muestra.shape[1])}
-    if len(objeto)==1:    
-        # Verificando los temas 
-        float_data = [not(isinstance(data, (int, float))) for data in muestra.iloc[3]]
-        saltos = 1+sum(float_data)
-        print(saltos)
-        return saltos
-    saltos=0
-    return saltos
-# Lectura archivos Excel
-# Input 
-path = '/opt/data_'
-filename = 'GIZ METEO PPC F1 & F2 20221009.xls'
-# Process
-fn = path+'/'+filename
-skip_rows = verify_date(fn)
-df = pd.read_excel(fn, skiprows=skip_rows)
-buffer = io.StringIO()
-df.info(buf=buffer)
-s = buffer.getvalue()
-with open("df_info.txt", "w", encoding="utf-8") as f:
-    f.write(s)
-# Output
-df.to_csv('/opt/airflow/tmp/temp_E.csv',index=False)
+from datetime import date
+'''
+Lectura de Base de datos para interpretar el esquema
+'''
+# Solicitud SQL de Tabla Parque
+
+# Bucle For Solicitud de ESQUEMA por parque
+'''
+Lectura del archivo CSV en el directorio SAMBA
+'''
+path = 'E:/CPERv/Centralizacion de datos/DataSCADA/' #SQL DB
+filename = 'Aero_10 minutos_6861_2024-01-30_2024-01-31.csv' #SQL DB
+skiprows = 0 # SQL DB
+source = path+'/'+filename
+df = pd.read_csv(source, skiprows=skiprows,sep=';', engine='python',quotechar='"', decimal=",", parse_dates=['Hora']).replace('"','')
+df.dropna(axis=1, how='all', inplace=True)
+df.to_feather("QII_"+str(date.today())+".feather")
+df.to_csv("QII_"+str(date.today())+".csv")
