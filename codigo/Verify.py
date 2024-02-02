2e6c294804f61eabbed7c87214ce4fb18fc47c20
JoseDevApps <Lionhard_2.6@hotmail.com>
2024-02-02 15:55:10 -0400

    Modified

--- codigo/Verify.py
+++ codigo/Verify.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import numpy as np
 from sqlalchemy import create_engine, MetaData, Table
 # input
 path = '/opt/airflow/tmp'
@@ -7,10 +6,10 @@ filename = 'temp_T.csv'
 fn = path + '/' + filename
 Host = 'db'
 Port = '5432'
-User = 'postgres'
-Password = 'cperv_db_solar'
-DBName = 'cperv'
-TableName = 'data_oruro'
+User = 'postgres_giz'
+Password = 'giz_peerzoip'
+DBName = 'django_gizb'
+TableName = 'data_enercon'
 # Process
     # Connection to the DB
 engine = create_engine('postgresql://'+User+':'+Password+'@'+Host+':'+Port+'/'+DBName)
@@ -22,7 +21,5 @@ try:
     print(table_db.columns.keys())
 except:
     df = pd.read_csv(fn,index_col=0, parse_dates=True)
-    print(df.info())
-    # print(df)
-    pd.io.sql.get_schema(df, name = 'data_oruro',)
-    df.head(0).to_sql(name='data_oruro', con=engine, if_exists='replace')
+    pd.io.sql.get_schema(df, name = 'data_enercon',)
+    df.head(0).to_sql(name='data_enercon', con=engine, if_exists='replace')
